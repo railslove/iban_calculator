ebcbb3ce30b3abbc441f24ee0ff241fceeb05cbd
Maximilian Schulz <m.schulz@kulturfluss.de>
2014-07-28 13:27:22 +0200

    Add rspec for testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'iban_calculator'
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 3
+
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
