5ec2390823663463bdd2f9c80be091946e74f714
Maximilian Schulz <m.schulz@kulturfluss.de>
2014-08-07 15:41:13 +0200

    Add method to validate IBANs

--- lib/iban_calculator.rb
+++ lib/iban_calculator.rb
@@ -8,17 +8,26 @@ require 'iban_calculator/bic_candidate'
 require 'iban_calculator/iban_validator_response'
 
 module IbanCalculator
+  # Extensions
   include ActiveSupport::Configurable
 
+  # Configuration
   config_accessor(:url) { 'https://ssl.ibanrechner.de/soap/?wsdl' }
   config_accessor(:user) { '' }
   config_accessor(:password) { '' }
   config_accessor(:logger) { Logger.new(STDOUT) }
 
+  # Errors
   ServiceError = Class.new(StandardError)
 
   def self.calculate_iban(attributes = {})
     client = IbanBic.new(config.user, config.password, config.url, config.logger)
     client.calculate_iban(attributes)
+  end
+
+  def self.validate_iban(iban)
+    client = Savon.client(wsdl: config.url, logger: config.logger)
+    response = client.call(:validate_iban, message: { iban: iban, user: config.user, password: config.password })
+    IbanValidatorResponse.new(response.body[:validate_iban_response][:return])
   end
 end
